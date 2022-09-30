de98f0070a94d1767683869dc9019e0c10bb6246
Cassidy Bell <witetrashd@gmail.com>
2022-09-30 11:09:58 +0700

    Fix host to server_host in client

--- client/client.py
+++ client/client.py
@@ -18,7 +18,7 @@ class Client:
         self.client.send(f"{security.access_key}>|<".encode())
         self.client.send(self.nickname.encode())
 
-        if self.host == 'localhost':
+        if self.server_host == 'localhost':
             print(f"client is running in test mode! {host}:{port}")
 
     def recieve_message(self):
